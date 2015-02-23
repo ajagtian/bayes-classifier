21f0966462d79a87b38cd0fc75e24211d1167ee7
Akshay Jagtiani <ajagtian@usc.edu>
2015-02-22 16:15:37 -0800

    bug_fix=out_file_formatting_for_Naive_Bayes

--- nbtrain.py
+++ nbtrain.py
@@ -12,34 +12,40 @@ def	readfile(filename):
 		return text.replace('\n',' ').replace('\r','');
 
 
-def	create_training_file(dir, dest_file):
+def	create_training_file(dir, dest_file, option):
 		"""creates a training file from a list of files, 
 		this training file can then be used for NB learning"""
 		
-		filenames = os.listdir(dir)
+		filenames = sorted(os.listdir(dir))
 		out_file_handle  = open(dest_file, 'w')
 		lines = []
 		for file in filenames:
 			filetext = readfile(os.path.join(dir, file))
-			lines = append_to_file(file, filetext, lines)
+			lines = append_to_file(file, filetext, lines, option)
 		out_file_handle.writelines(lines)
 		out_file_handle.close()
 
 
-def	append_to_file(filename, filetext, lines):
+def	append_to_file(filename, filetext, lines, option):
 		"""appends a line to the training file"""
-		lines.append(filename[:filename.find('.')]+' '+filetext+'\n')		
-		## to create a test file, uncomment following line, and comment the above one
-		#lines.append(filetext+'\n')
+		if option == '-train':
+			lines.append(filename[:filename.find('.')]+' '+filetext+'\n')		
+		if option == '-test':
+			lines.append(filetext+'\n')
+	
 		return lines
 		
 
 def	main():
 		args = sys.argv[1:]
-		if len(args) != 2:
-			print('usage: ./nbtrain.py <source_directory> <training_file_name>')
+		if len(args) < 2:
+			print('usage: ./nbtrain.py [-test|-train] <source_directory> <training_file_name>')
 			sys.exit(0)
-		create_training_file(args[0], args[1])
+		if len(args) == 2:
+			create_training_file(args[0], args[1], '-train')
+		else:
+			option = args[0]
+			create_training_file(args[1], args[2], option)
 		
 
 if __name__ == '__main__':
